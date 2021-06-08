6e4fc55e751e65b0b3594d5c6c5f327ef178394f
gb-0001 <82384062+gb-0001@users.noreply.github.com>
2021-06-08 16:37:00 +0200

    Update install_minikube.sh

--- kubernetes/install_minikube.sh
+++ kubernetes/install_minikube.sh
@@ -7,7 +7,7 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 #install kubectl pour linux
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-sudo apt intall -y conntrack
+sudo apt install -y conntrack
 
 #Start a cluster using the docker driver
 minikube start --driver=none
