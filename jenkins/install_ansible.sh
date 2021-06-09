4cfb5807bdb9a286c9cb90b8b94d73f9df8294da
gb-0001 <gb-0001@outlook.fr>
2021-06-09 13:26:51 +0200

    maj install ansible ajout playbook

--- jenkins/install_ansible.sh
+++ jenkins/install_ansible.sh
@@ -16,6 +16,9 @@ install_package "sshpass"
 sudo mkdir /home/vagrant/ansible
 cd /home/vagrant/ansible && wget -O hosts https://github.com/arnauddegez/tp_juin_ci-cd/raw/main/ansible/hosts
 cd /home/vagrant/ansible && wget -O playbook.yml https://github.com/arnauddegez/tp_juin_ci-cd/raw/main/ansible/playbook.yml
+cd /home/vagrant/ansible && wget -O maj_k8sfile.yml https://github.com/arnauddegez/tp_juin_ci-cd/raw/main/ansible/maj_k8sfile.yml
+cd /home/vagrant/ansible && wget -O maj_svck8sfile.yml https://github.com/arnauddegez/tp_juin_ci-cd/raw/main/ansible/maj_svck8sfile.yml
 
 #pour le fonctionnement de sshpass avec ansible
 sudo sed -i 's/#host_key_checking = False/host_key_checking = False/g' /etc/ansible/ansible.cfg
+
